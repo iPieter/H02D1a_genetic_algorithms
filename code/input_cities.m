5365f783bdb0539d90862f89bb1933b2a67a1dc4
Pieter Depamelaere <pieter.depamelaere@hotmail.com>
2018-10-30 19:32:37 +0100

    PDP: Added intermediate method to change the current figure, because otherwise, the gui keeps stealing the focus.

--- code/input_cities.m
+++ code/input_cities.m
@@ -1,6 +1,10 @@
 function [x y] = input_cities(ncities)
         % get the input cities
-        fg1 = figure(1);clf;
+        %PDP: The statement below makes the tspGUI keep on stealing the
+        %focus, which is not practical. https://stackoverflow.com/questions/8488758/inhibit-matlab-window-focus-stealing
+        %fg1 = figure(1);clf;
+        fg1 = change_current_figure(1);clf;
+        
         %subplot(2,2,2);
         axis([0 1 0 1]);
         title(ncities);
@@ -15,4 +19,8 @@ function [x y] = input_cities(ncities)
         end
         hold off;
         set(fg1, 'Visible', 'off');
+end
+
+function change_current_figure(h)
+        set(0,'CurrentFigure',h)
 end
